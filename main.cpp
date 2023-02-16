987c332ddc98e728188bfdb5ad1e55b16b0b1f2f
Андрій Грицюк <venganpro12@gmail.com>
2023-02-16 21:34:10 +0200

    added working remove_if()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include "std.h"
 
+template<typename T>
+void print(const T& element, const string& text) {
+	cout << text << "\n";
+	for (auto& e : element) {
+		cout << " " << e;
+	} cout << "\n\n";
+}
+
 int main()
 {
-	list<string> list{ "Andrew", "Yevhenii", "Max", "Yehor" };
-	
+	list<string> list1{ "Andrew", "Yevhenii", "Max", "Yehor" };
+	list1.remove_if([](string i) { if (i == "Andrew") return true; return false; });
+	print(list1, " >>> list1 remove_if <<<");
 
 	return 0;
 }
