04cacf61984b8dafc2728ff0bd98fc9c04a80cbd
zheni4ka <ebranik15@gmail.com>
2023-02-16 22:25:16 +0200

    андрій егор(грішник) і макс, заливайте сюда свої роботи

--- main.cpp
+++ main.cpp
@@ -1,14 +1,60 @@
-#include "std.h"
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <deque>
+using namespace std;
 
-int sum(int num1, int num2) { return num1 + num2; }
-int dob(int num1, int num2) { return num1 * num2; }
+bool num(const string& str)
+{
+    return str.size() > 5;
+}
+
+string MakeEmrtyLetter(string a)
+{
+    a[a.size() - 1] = ' ';
+    return a;
+}
 
 int main()
 {
-	cout << " 4 + 6 = " << sum(4, 6) << "\n";
-	cout << " 2 * 5 = " << dob(2, 5) << endl;
-	return 0;
-	
-	// тут був женя)))
-	// тут був макс
+    //TASK 1
+    list <string> a{ "Andrii", "Egor", "Maxim", "Artem", "Oleg", "Zhenya" };
+    char tmp2;
+    cin >> tmp2;
+
+    auto it = find_if(begin(a), end(a), [tmp2](const string& str) {
+
+        return str[0] == tmp2;
+
+        });
+
+    cout << distance(begin(a), it);
+
+    //task 2
+    cout << "number of word which has more than 5 letters " << count_if(begin(a), end(a), num);
+
+
+
+    // task 3
+    auto it = max_element(begin(a), end(a), [](const string& str1, const string& str2) {
+        return str1.size() < str2.size();
+        });
+
+    cout << *it << endl;
+
+    it = min_element(begin(a), end(a), [](const string& str1, const string& str2) {
+        return str1.size() < str2.size();
+        });
+
+
+    cout << *it;
+
+    deque<string> b{ "Andrii", "Egor", "Maxim", "Artem", "Oleg", "Zhenya" };
+
+    transform(begin(b), end(b), begin(b), MakeEmrtyLetter);
+
+    for (auto& i : b)
+    {
+        cout << i << endl;
+    }
 }
