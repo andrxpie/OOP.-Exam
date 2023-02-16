22da93a507205aa912c53f40c84fa2a4867a847d
flexxdarker <ksenzhakmax@gmail.com>
2023-02-16 19:52:03 +0200

    added funct dob

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 int sum(int num1, int num2) { return num1 + num2; }
 
+int dob(int num1, int num2) { return num1 * num2; }
+
 int main()
 {
 	cout << " 4 + 6 = " << sum(4, 6) << "\n";
-
+	cout << " 2 * 5 = " << dob(2, 5) << endl;
 	return 0;
 	
 	//тут був женя)))
