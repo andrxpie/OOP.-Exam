37f0573c3be32eb11bf758ccec617df12aeb0421
Андрій Грицюк <venganpro12@gmail.com>
2023-02-16 22:01:17 +0200

    complete work

--- main.cpp
+++ main.cpp
@@ -8,11 +8,26 @@ void print(const T& element, const string& text) {
 	} cout << "\n\n";
 }
 
+string toUpStr(string& element) {
+	transform(begin(element), end(element), begin(element), toupper);
+	return element;
+}
+
 int main()
 {
 	list<string> list1{ "Andrew", "Yevhenii", "Max", "Yehor" };
-	list1.remove_if([](string i) { if (i == "Andrew") return true; return false; });
+	string deleteWord; cout << " word: "; cin >> deleteWord;
+	list1.remove_if([deleteWord](string i) { if (i == deleteWord) return true; return false; });
 	print(list1, " >>> list1 remove_if <<<");
+
+	// <---CAPS--->
+	transform(begin(list1), end(list1), begin(list1), toUpStr);
+	print(list1, " >>> list1 upper <<<");
+
+	// <---write-file--->
+	ofstream file("saved.txt");
+	ostream_iterator<string> fileIt(file, "\n");
+	copy(begin(list1), end(list1), fileIt);
 
 	return 0;
 }
