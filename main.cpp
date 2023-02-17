d5a480587e74946af6c327d129d4ec126c093b93
flexxdarker <125499561+flexxdarker@users.noreply.github.com>
2023-02-17 16:39:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ bool AreSame(string a, string b)
 }
 int main()
 {
+	cout<<"task 12: "<<endl;
 	list<string> list2;
 	ifstream ifile ("list.txt");
 	string tmp;
@@ -23,10 +24,12 @@ int main()
 	}
 	printCont(list2);
 	list<string> list3{"Yehor", "Andriy", "Jenya", "Jenya", "Max"};
+	cout<<"task 13: "<<endl;
 	list<string> list(list2.size() + list3.size());
 	//merge(begin(list2),end(list2),begin(list3),end(list3), list.begin());
 	//cout << "Two lists" << endl;
 	//printCont(list);
+	cout<<"tasl 14: "<<endl; 
  	unique(begin(list3),end(list3), AreSame);
 	cout << "list unique"<<endl;
 	printCont(list3);
