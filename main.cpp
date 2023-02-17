337912739ee591b2bf50d7854129c5be4fc41e6c
flexxdarker <125499561+flexxdarker@users.noreply.github.com>
2023-02-17 17:03:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "std.h"
 template<typename T>
-void printCont(const T& str)
+
+void print(const T& str)
 {
 	for (auto& i : str)
 	{
@@ -8,29 +9,33 @@ void printCont(const T& str)
 	}cout << endl;
 
 }
+
 bool AreSame(string a, string b)
 {
 	return a == b;
 }
+
 int main()
 {
-	cout<<"task 12: "<<endl;
+	// <--write-data-from-file-->
 	list<string> list2;
-	ifstream ifile ("list.txt");
+	ifstream inFile ("list.txt");
 	string tmp;
-	while (ifile >> tmp)
+	while (inFile >> tmp)
 	{
 		list2.push_back(tmp);
 	}
-	printCont(list2);
+	print(list2);
+	
 	list<string> list3{"Yehor", "Andriy", "Jenya", "Jenya", "Max"};
-	cout<<"task 13: "<<endl;
-	list<string> list(list2.size() + list3.size());
+	// <--merge-->
+	//list<string> list(list2.size() + list3.size());
 	//merge(begin(list2),end(list2),begin(list3),end(list3), list.begin());
 	//cout << "Two lists" << endl;
-	//printCont(list);
-	cout<<"tasl 14: "<<endl; 
+	//print(list);
+	
+	// <--delete-same-words-->
  	unique(begin(list3),end(list3), AreSame);
 	cout << "list unique"<<endl;
-	printCont(list3);
+	print(list3);
 }
